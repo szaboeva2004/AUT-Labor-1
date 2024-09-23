fce58df5fcac42044adf207d9a4cdea680838c61
Szabó Éva Mária <szaboevamaria@edu.bme.hu>
2024-09-23 17:42:54 +0200

    Task2 done

--- diversity/build.gradle.kts
+++ diversity/build.gradle.kts
@@ -3,46 +3,25 @@ plugins {
     jacoco
     application
     `java-library`
+    id("hu.bme.mit.ase.shingler.gradle.java")
+
 }
 
-java.toolchain {
-    languageVersion.set(JavaLanguageVersion.of(21))
-}
 
-repositories {
-    mavenCentral()
-}
-
-val picoCliVersion = "4.7.6"
-val slf4jVersion = "1.7.36"
-val log4jVersion = "2.23.1"
-val junitVersion = "5.10.0"
 
 dependencies {
-    implementation("org.slf4j:slf4j-api:$slf4jVersion")
-    implementation("info.picocli:picocli:$picoCliVersion")
+    implementation(libs.slf4j.api)
+    implementation(libs.picocli)
 
-    runtimeOnly("org.apache.logging.log4j:log4j-slf4j-impl:$log4jVersion")
+    runtimeOnly(libs.slf4j.log4j.impl)
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
-
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitVersion")
+    testImplementation(libs.junit.jupiter.core)
+    testRuntimeOnly(libs.junit.jupiter.engine)
 
     api(project(":lib"))
     api(project(":logic"))
 }
 
-tasks {
-    test {
-        useJUnitPlatform()
-        testLogging.showStandardStreams = true
-        finalizedBy(jacocoTestReport)
-    }
-
-    jacocoTestReport {
-        inputs.files(test.get().outputs)
-    }
-}
 
 application {
     mainClass = "hu.bme.mit.ase.shingler.similarity.SimilarityApp"
